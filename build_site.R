1fe06b8aac2b8f9ef337f0f1943223bacb2425d1
sfick <stephen.fick@gmail.com>
2023-07-16 18:50:48 -0700

    Simplify Site UI

--- build_site.R
+++ build_site.R
@@ -17,17 +17,33 @@ generate_page <- function(pagename, title, subtitle = '', headerimage = 'images/
 ##################
 ## make index page
 
-idx = generate_page('Steve Fick', 'STEVE FICK', 'Geospatial Data Scientist', 'images/background.jpg')
+#idx = generate_page('Steve Fick', 'STEVE FICK', 'Geospatial Data Scientist', 'images/background.jpg')
+idx = generate_page('Steve Fick', 'STEVE FICK', 'Geospatial Data Scientist')
 
 
 ## index page content
 
 idx_content = '
-<div class="w3-content w3-padding-large w3-margin-top" id="portfolio">
-    <div class="main">
-        <img class="w3-image" src="images/steve2bwborder.JPG" alt="Me" width="150" height="60"><br>
-             I develop tools and datasets for evidence-based decision making. My areas of specialization include environmental modeling, data science, spatial ecology, and restoration ecology.
-            <br><br><a href="https://scholar.google.com/citations?user=Y4h2OOoAAAAJ&hl=en">
+<div class="w3-content w3-padding-large" id="portfolio">
+    
+
+
+
+	<div class="main">
+
+
+	<div id="greeter">
+	
+        <div class="textbox"> 
+	<h1> Steve Fick </h1> <br>           
+	I develop tools and datasets for evidence-based decision making. My areas of specialization include environmental modeling, data science, spatial ecology, and restoration ecology.
+            <br><br>
+</div>
+<img class="avatar" src="images/steve2bwborder.JPG" alt="Me"><br>
+	
+</div>
+
+<a href="https://scholar.google.com/citations?user=Y4h2OOoAAAAJ&hl=en">
                 <i class="ai ai-google-scholar ai-1x"></i></a>   
                 <a href="https://github.com/fickse"<i class="fa fa-github fa-4"></i></a> <a href="https://fickse.wordpress.com/">blog</a>    
                 <a href="https://www.linkedin.com/in/steve-fick-334652195/"> linkedin </a>
@@ -88,7 +104,7 @@ cv_content = '
 <div class="w3-content w3-padding-large w3-margin-top" id="portfolio">
 
   <!-- Images (Portfolio) -->
-  <embed src="assets/cv.pdf" width="800px" height="2100px" />
+  <embed class="cv" src="assets/cv.pdf" width="800px" height="2100px" />
 
 </div>
 '
@@ -102,78 +118,57 @@ cat( cv, file = 'cv.html', sep = '\n')
 ## Make Projects Page
 proj <- generate_page('projects', 'PROJECTS')
 
-proj_content <- '
+
+add_content <- function(img, text){
+	
+	glue::glue('
+	<div id="greeter">
+	<img src="{img}" alt="thumb" class="avatar"></img>
+	<div class="textbox">
+	{text}<br><br>
+	</div>
+	</div>
+	')
+}
+
+projects = list(
+	list(
+		"img" = "images/thumb.PNG",
+		"text" = "Using geospatial data and historical records to evaluate conservation and restoration practice effectiveness across the Upper Colorado River Basin"
+	),
+
+	list(
+		"img" = "images/thumb_ic.jpg",
+		"text" = 'Working with <a href="https://www.zekebaker.com/projects">social scientists</a> to study how <a href="https://sciencemoab.org/climbersplace/">rock climbers at Indian Creek</a> (Bears Ears) evaluate their cultural and ecological impacts in the context of land-use conflict, new technologies and the burgeoning growth of the outdoor recreation industry.'
+	),
+
+	list(
+		"img" = "images/704_2016_b.jpg",
+		"text" = 'Testing <a href="https://www.usgs.gov/centers/sbsc/science/new-approaches-restoring-colorado-plateau-grasslands?qt-science_center_objects=0#qt-science_center_objects">novel restoration practices</a> for degraded arid lands with field experiments.'
+	),
+	list(
+		"img" = "images/trase.PNG",
+		"text" = 'Linking consumers, exporters, and production geographies in the global trade of forest-risk commodities with a <a href="https://www.trase.earth">supply-chain traceability tool</a> (trase).'
+	),
+	list(
+		"img" = "images/WC2.PNG",
+		"text" = '<a href="http://worldclim.org/version2">WorldClim 2</a>: A global climatology dataset updated with data from new stations and satellite archives'
+	)
+
+)		
+
+
+
+proj_content <- c('
 <div class="w3-content w3-padding-large w3-margin-top" id="portfolio">
+<div class="main">')
 
-  <!-- Images (Portfolio) -->
-<div class="main">
-<table>
-  <tr>
-   <td>
-   <img src="images/thumb.PNG" alt="thumb" style="max-width:160px" class="w3-image" /> <br>
-   </td>
-   
-   <td>
-   Using geospatial data and historical records to evaluate conservation and restoration practice effectiveness across the Upper Colorado River Basin
-   </td>
-   </tr>
-   
-  <tr>   
-    <td>
-      <img src="images/thumb_ic.jpg" alt="thumb" style="max-width:160px" />
-    </td>
-    
-    <td>
-      Working with <a href="https://www.zekebaker.com/projects">social scientists</a> to study how <a href="https://sciencemoab.org/climbersplace/">rock climbers at Indian Creek</a> (Bears Ears) evaluate their cultural and ecological impacts in the context of land-use conflict, new technologies and the burgeoning growth of the outdoor recreation industry.
-    </td>
-  </tr>
+for (p in projects){
+	proj_content = c(proj_content, add_content( p$img, p$text))
+}
 
-  <tr>
-    <td>
-        <img src="images/704_2016_b.jpg" alt="thumb" style="max-width:160px" />
-    </td>
-    <td>
-      Testing <a href="https://www.usgs.gov/centers/sbsc/science/new-approaches-restoring-colorado-plateau-grasslands?qt-science_center_objects=0#qt-science_center_objects">novel restoration practices</a> for degraded arid lands with field experiments.</li>
-    </td>
-  </tr>
-
-  <tr>
-    <td>
-      <img src="images/trase.PNG" alt="thumb" style="max-width:160px" />
-    </td>
-    <td>
-      Linking consumers, exporters, and production geographies in the global trade of forest-risk commodities with a <a href="https://www.trase.earth">supply-chain traceability tool</a> (trase).
-    </td>
-  </tr>
-  
-  <tr>
-    <td>
-      <img src="images/WC2.PNG" alt="thumb" style="max-width:160px" />
-    </td>
-    <td>
-      <a href="http://worldclim.org/version2">WorldClim 2</a>: A global climatology dataset updated with data from new stations and satellite archives
-    </td>
-  </tr>
-  </table>
-  
-
-  </div>
-
-</div>
-
-  
-
-</div>
-
-  </div>
-
-</div>
-
-
-<!-- End page content -->
-</div>
-'
-
+proj_content <- c(proj_content, '</div></div>')
+proj_content = paste(proj_content, collapse = ' ')
 proj <- gsub('<!--CONTENT-->', proj_content, proj)
 proj <- paste(proj, collapse = '\n')
 cat( proj, file = 'projects.html', sep = '\n')
